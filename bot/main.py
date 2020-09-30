cff5aea8734de34687b8f3a10cbcb876ac25abd8
Lewis Stokes <stokesl4@uni.coventry.ac.uk>
2020-09-30 22:07:29 +0100

    Initial commit

---
+++ bot/main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+cli = commands.Bot(command_prefix=".")
+token = os.getenv("bot_token")
+
+@client.event
+async def on_ready():
+    await client.change_presence(
+        status = discord.Status.idle, 
+        activity = discord.Game("Listening to .help")
+    )
+    print("I am online")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong!")
+    
