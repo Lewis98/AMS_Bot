dea09c0d246a0c313a3e6dec2ca03c4c10ad226d
Lewis Stokes <stokesl4@uni.coventry.ac.uk>
2020-10-02 10:18:40 +0100

    Added client check foundations

--- bot/main.py
+++ bot/main.py
@@ -2,14 +2,13 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix="♪")
 token = os.getenv("bot_token")
 
 @client.event
 async def on_ready():
     await client.change_presence(
-        status = discord.Status.idle, 
-        activity = discord.Game("Listening to .help")
+        status = discord.Status.online,
     )
     print("I am online")
 
@@ -17,6 +16,18 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Pong!")
+
+@client.command(pass_context = True)
+async def checkMe(ctx):
+    user = ctx.message.author
+    await ctx.send(f"Checking " + str(user) + "!")
+
+    # Check Student ID
+    
+    valid = True
+    if valid:
+        role = discord.utils.get(user.server.roles, name="Paid Member")
+        await client.add_roles(user, role)
 
 
 client.run(token)
